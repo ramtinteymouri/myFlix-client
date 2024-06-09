7f468cb1595e3443be325439bd244e05b84697cd
ramtinteymouri <ramtinteymouri@gmail.com>
2024-06-09 09:57:42 -0700

    added components to have movie view functionality

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
+
+export const MainView = () => {
+  const [movies, setMovies] = useState([
+    { id: 1, title: "Pulp Fiction", image: "https://fakeimg.pl/100x150?text=Pup+Fiction", director:"Quentin Tarantino" },
+    { id: 2, title: "The Shawshank Redemption", image: "https://fakeimg.pl/100x150?text=The+Shawshank+Redemption", director:"Frank Darabont" },
+    { id: 3, title: "The Godfather", image: "https://fakeimg.pl/100x150?text=The+Godfather", director:"Francis Ford Coppola" },
+    { id: 4, title: "The Dark Knight", image: "https://fakeimg.pl/100x150?text=The+Dark+Knight", director:"Christopher Nolan" },
+    { id: 5, title: "12 Angry Men", image: "https://fakeimg.pl/100x150?text=12+Angry+Men", director:"Sidney Lumet" }
+  ]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  if (selectedMovie) {
+    return <MovieView movie={selectedMovie} onMovieClick={() => {selectedMovie(null)}}/>;
+  }
+
+  if (movies.length === 0) {
+    return <div>The list is empty!</div>;
+  }
+
+  return (
+    <div>
+      {movies.map((movie) => (
+        <MovieCard key={movie.id} movie={movie} onMovieClick={(newSelectedMovie) => {
+          setSelectedMovie(newSelectedMovie);
+        }}/>
+      ))}
+    </div>
+  );
+};
