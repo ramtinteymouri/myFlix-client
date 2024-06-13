fa457eb45a4d3c1e0ce255756e7c8f230cc4c556
ramtinteymouri <ramtinteymouri@gmail.com>
2024-06-12 17:44:14 -0700

    added prop-types to validate the data

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,6 +1,27 @@
+import PropTypes from "prop-types";
 export const MovieCard = ({movie, onMovieClick}) => {
   return (<div onClick={() => {
     onMovieClick(movie);
   }}>{movie.title}</div>
   );
+};
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    genre: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+    }),
+    director: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      nationality: PropTypes.string.isRequired,
+      bio: PropTypes.string.isRequired
+    }),
+    additionalAttributes: PropTypes.shape({
+      runtime: PropTypes.number.isRequired,
+      mainActors: PropTypes.array.isRequired
+    }),
+    releaseYear: PropTypes.number.isRequired
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired,
 };
