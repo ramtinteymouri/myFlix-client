baf6759af9e6d0988fbc83c6505550035b4985ca
ramtinteymouri <ramtinteymouri@gmail.com>
2024-07-15 17:32:48 -0700

    updated login view to get the usename

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -24,6 +24,7 @@ export const LoginView = ({onLoggedIn}) => {
                 if (data.user) {
                     localStorage.setItem("user", JSON.stringify(data.user));
                     localStorage.setItem("token", data.token);
+                    localStorage.setItem("username", data.user.username);
                     onLoggedIn(data.user, data.token);
                 }
                 else {
